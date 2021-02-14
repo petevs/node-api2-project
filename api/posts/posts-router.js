a9e08f3072d000552908ce7994a5e2df47816d0b
Pete VS <pete_vs88@me.com>
2021-02-14 09:07:39 -0500

    update post done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -60,4 +60,33 @@ router.post('/api/posts', (req,res) => {
 
 })
 
+//Update a post
+
+router.put('/api/posts/:id', ( req, res ) => {
+
+    if(!req.body.title || !req.body.contents){
+        return res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }
+
+    posts.update(req.params.id, req.body)
+        .then((post) => {
+            if(post){
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: "The post information could not be modified"
+            })
+        })
+
+})
+
 module.exports = router
