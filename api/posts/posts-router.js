772ce9f4381b725cead888600de6a3a3e2eb4c53
Pete VS <pete_vs88@me.com>
2021-02-14 09:03:20 -0500

    post post done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 // implement your posts router here
 
+//GET ALL POSTS
 router.get('/api/posts', ( req, res ) => {
     posts.find()
         .then((posts) => {
@@ -17,6 +18,7 @@ router.get('/api/posts', ( req, res ) => {
         })
 })
 
+//GET POST BY ID
 router.get('/api/posts/:id', ( req, res ) => {
     posts.findById(req.params.id)
         .then((post) => {
@@ -34,6 +36,28 @@ router.get('/api/posts/:id', ( req, res ) => {
                 message: "The post information could not be retrieved"
             })
         })
+})
+
+//POST NEW POST
+
+router.post('/api/posts', (req,res) => {
+    if(!req.body.title || !req.body.contents){
+        return res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }
+
+    posts.insert(req.body)
+        .then((post) => {
+            res.status(201).json(post)
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: "There was an error while saving the post to the database"
+            })
+        })
+
 })
 
 module.exports = router
