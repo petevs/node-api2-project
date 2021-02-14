1b8bc800386963ef029c50b721a109b5f7833967
Pete VS <pete_vs88@me.com>
2021-02-14 08:54:21 -0500

    posts router connected

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,20 @@
+const express = require('express')
+const posts = require('./posts-model')
+const router = express.Router()
+
 // implement your posts router here
+
+router.get('/api/posts', ( req, res ) => {
+    posts.find()
+        .then((posts) => {
+            res.status(200).json(posts)
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: "Error getting posts"
+            })
+        })
+})
+
+module.exports = router
